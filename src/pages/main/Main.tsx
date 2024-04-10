5e47f4d71f66570ba5bec7b4ae624dc7c94a8c4a
naeun-shin <3020naaeun@gmail.com>
2024-04-10 22:17:08 +0900

    [Feat] 쿠기 설정

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -14,6 +14,7 @@ import ListTitle from '@/components/commons/mainItem/ListTitle';
 import MainList from '@/components/commons/mainItem/MainList';
 import { useNavigate } from 'react-router-dom';
 // import { ResultsSection } from '@/components/search/Search.style';
+import { Cookies } from 'react-cookie';
 
 interface MainProps {
   onClick: () => void;
@@ -60,6 +61,9 @@ const Main: React.FC<MainProps> = () => {
     likes: 77,
     imageUrl: '이미지URL',
   }));
+
+  const cookies = new Cookies();
+  console.log('cookies >>> main >> ', cookies.get('Authorization'));
 
   return (
     <>
