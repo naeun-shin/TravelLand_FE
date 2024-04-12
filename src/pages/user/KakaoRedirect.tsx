7e8fe0e4ec040f44a2edcc460c795024839c6428
naeun-shin <3020naaeun@gmail.com>
2024-04-12 22:27:19 +0900

    [fix] response console

--- src/pages/user/KakaoRedirect.tsx
+++ src/pages/user/KakaoRedirect.tsx
@@ -1,24 +1,24 @@
 import { useEffect } from 'react';
 import { instance } from '@/api/axios';
 import { useNavigate } from 'react-router-dom';
-import { Cookies } from 'react-cookie';
+// import { Cookies } from 'react-cookie';
 
 const KakaoRedirect = () => {
-  const cookie = new Cookies();
+  // const cookie = new Cookies();
   const navigate = useNavigate();
   const code = new URL(window.location.href).searchParams.get('code');
-
+  console.log(code);
   const response = async (code: string | null) => {
     try {
-      await instance
-        .get('/users/login/kakao', {
-          params: { code },
-        })
-        .then((res: any) => {
-          console.log(res);
-          console.log(res.headers.getAuthorization);
-          cookie.set('Authorization', res.headers.getAuthorization);
-        });
+      const response = await instance.get('/users/login/kakao', {
+        params: { code },
+      });
+      console.log('response >> ', response);
+      // .then((res: any) => {
+      //   console.log(res);
+      //   console.log(res.headers.getAuthorization);
+      //   cookie.set('Authorization', res.headers.getAuthorization);
+      // });
       navigate('/');
     } catch (error) {
       console.log(error);
