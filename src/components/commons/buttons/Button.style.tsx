1f7446671e019f5eccf4f1526fda42c507824e16
better_320 <70196050+naeun-shin@users.noreply.github.com>
2024-04-17 00:09:50 +0900

    [Fix] Button.style.tsx 중괄호 수정

--- src/components/commons/buttons/Button.style.tsx
+++ src/components/commons/buttons/Button.style.tsx
@@ -23,10 +23,10 @@ export const StyledButton = styled.button<ButtonProps>`
   text-align: center;
   text-decoration: none;
   &:hover {
-    background-color: ${(props) =>
-      props.hoverColor || props.color}; // 호버 시 색상 변경
+    background-color: ${(props) => props.hoverColor || props.color}; // 호버 시 색상 변경
   }
   margin-right: ${(props) => props.marginRight || '0px'};
+`;
 
 export const ButtonContainer = styled.div`
   display: flex;
