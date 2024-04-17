ff3f2c037cb99f50fa3783587a376bde483d0719
naeun-shin <3020naaeun@gmail.com>
2024-04-17 19:09:06 +0900

    [Fix]로그인 수정

--- src/pages/user/KakaoRedirect.tsx
+++ src/pages/user/KakaoRedirect.tsx
@@ -3,62 +3,33 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import { Cookies } from 'react-cookie';
 
 const KakaoRedirect: React.FC = () => {
-  const cookie = new Cookies();
   const navigate = useNavigate();
   const location = useLocation();
 
-  const queryParams = new URLSearchParams(location.search);
-  const authorizationToken = queryParams.get('Authorization');
-
-  // const params = useParams();
-  // const { Authorization } = params;
-
-  console.log('params >> ', queryParams);
-  console.log('param Authorization >> ', authorizationToken);
-
   useEffect(() => {
-    // const authToken = res.headers['authorization']; // Headers are generally case-insensitive but depend on server configuration
-    if (authorizationToken) {
-      cookie.set('Authorization', authorizationToken);
-    } else {
-      console.log('Authorization token not found in the response headers.');
-    }
-  });
-  navigate('/');
+    const queryParams = new URLSearchParams(location.search);
+    const authorizationToken = queryParams.get('Authorization');
+    const cookie = new Cookies();
 
-  // const navigate = useNavigate();
-  // const code = new URL(window.location.href).searchParams.get('code');
-  // const response = async (code: string | null) => {
-  //   if (!code) {
-  //     console.log('Authorization code not found.');
-  //     navigate('/'); // Navigate to home if no code is found
-  //     return;
-  //   }
-  //   try {
-  //     const res: AxiosResponse = await instance.get('/users/login/kakao', {
-  //       params: { code },
-  //     });
-  //     // console.log(res);
-  //     // Correct way to access custom headers - make sure the header names are correctly handled
-  //     const authToken = res.headers['authorization']; // Headers are generally case-insensitive but depend on server configuration
-  //     if (authToken) {
-  //       cookie.set('Authorization', authToken, { path: '/' });
-  //     } else {
-  //       console.log('Authorization token not found in the response headers.');
-  //     }
-  //     navigate('/');
-  //   } catch (error: any) {
-  //     console.error(
-  //       'An error occurred during the authentication process:',
-  //       error,
-  //     );
-  //     navigate('/');
-  //   }
-  // };
-  // useEffect(() => {
-  //   response(code);
-  // }, [code]);
-  return null; // Since this component does not render anything, return null
+    if (authorizationToken) {
+      // 쿠키 옵션을 설정할 수 있습니다. 예를 들어, secure: true로 설정할 경우 HTTPS를 통해서만 쿠키가 전송됩니다.
+      const options = {
+        path: '/',
+        // secure: true, //secure: true로 설정할 경우 HTTPS를 통해서만 쿠키가 전송
+        // httpOnly: true,
+        // sameSite: 'strict',
+        // expires: /* 쿠키 만료 날짜를 설정할 수 있습니다. */
+      };
+
+      cookie.set('Authorization', authorizationToken, options);
+      navigate('/'); // 메인 페이지로 이동
+    } else {
+      console.error('Authorization token not found in the query string.');
+    }
+    // 의존성 배열에 location.search를 추가하여 해당 값이 변경될 때만 이 효과가 실행되도록 합니다.
+  }, [location.search, navigate]);
+
+  return null; // 이 컴포넌트는 아무것도 렌더링하지 않으므로 null을 반환합니다.
 };
 
 export default KakaoRedirect;
