9843f31c8e04d6b716c8c08a0fc0ee59c4ad06a7
naeun-shin <3020naaeun@gmail.com>
2024-04-17 19:31:00 +0900

    [Fix]마이페이지 에러 발생으로 임시 주석처리

--- src/components/commons/user/myPage/myPageComponents/MyPageUserInfo.tsx
+++ src/components/commons/user/myPage/myPageComponents/MyPageUserInfo.tsx
@@ -1,13 +1,13 @@
 // import { useGetUerInfoQuery } from '@/hooks/useQuery';
-import { useGetUerInfoQuery } from '@/hooks/useQuery';
+// import { useGetUerInfoQuery } from '@/hooks/useQuery';
 import * as S from '@components/commons/user/myPage/MyPage.style';
 
 const MyPageUserInfo = () => {
-  const { data, isLoading, isError } = useGetUerInfoQuery();
-  console.log(data);
+  // const { data, isLoading, isError } = useGetUerInfoQuery();
+  // console.log(data);
 
-  if (isLoading) return <div>Data is Loading</div>;
-  if (isError) return <div>Error occurred during fetching</div>;
+  // if (isLoading) return <div>Data is Loading</div>;
+  // if (isError) return <div>Error occurred during fetching</div>;
   return (
     <>
       <S.MyPageContainer>
