94d1e28eee461c94e7c2418f5ccc71161e493c5b
naeun-shin <3020naaeun@gmail.com>
2024-04-17 19:00:06 +0900

    [Fix]마이 페이지 수정 및 로그인 로직 수정

--- src/components/commons/Lists/List.style.tsx
+++ src/components/commons/Lists/List.style.tsx
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+interface IsPublicTitleProps {
+  isPublic: boolean;
+}
 
 const ListContainer = styled.div`
   display: flex;
@@ -93,6 +96,103 @@ const ListItemRight = styled.div`
   align-items: center;
 `;
 
+const MyPlanListWrapper = styled.div`
+  /* width: 80%; */
+  /* border: 1px solid gray; */
+  /* padding: 50px 0px; */
+  font-size: 18px;
+`;
+
+const MyPlanListButtons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const MyPlanListItemWrapper = styled.div`
+  padding: 30px;
+  /* border: 1px solid gray; */
+  /* display: flex; */
+`;
+
+const MyPlanListItems = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  /* justify-content: space-between; */
+
+  height: 100px;
+
+  border-bottom: 1px solid lightGray;
+  padding: 5px;
+`;
+
+const MyPlanIsPublic = styled.div<IsPublicTitleProps>`
+  display: flex;
+  justify-content: center;
+
+  width: 15%;
+
+  cursor: pointer;
+
+  font-size: 18px;
+  color: ${(props: { isPublic: any }) => (props.isPublic ? '#5AC8EC' : 'gray')};
+`;
+
+const MyPlanListContentBox = styled.div`
+  width: 50%;
+  div {
+    color: gray;
+  }
+`;
+
+const MyPlanListTitle = styled.span`
+  padding-top: 15px;
+  font-size: 22px;
+  /* font-weight: bold; */
+  color: black;
+`;
+
+const MyPlanListCountBox = styled.div`
+  height: 60%;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  width: 15%;
+  border-right: 1px solid lightGray;
+  color: gray;
+
+  div {
+    margin: 5px;
+    display: flex;
+    padding: 0px 5px;
+  }
+`;
+
+const MyPlanListInviteeBox = styled.div`
+  display: flex;
+  position: relative;
+  height: 50px; // This should be the height of the largest image
+  width: 20%;
+
+  img {
+    width: 35px; // Adjust the width as needed
+    height: 35px; // Adjust the height as needed
+    border-radius: 50%;
+    border: 2px solid white; // Adds a white border to distinguish overlapping images
+    position: absolute;
+
+    &:first-child {
+      left: 0;
+    }
+    &:nth-child(n + 2) {
+      left: calc(
+        25px * (n - 1)
+      ); // Adjust the overlay by changing 25px to the desired value
+    }
+  }
+`;
+
 export {
   ListContainer,
   ListCity,
@@ -106,4 +206,13 @@ export {
   ListItemLeft,
   ListLeftIcon,
   ListItemRight,
+  MyPlanListWrapper,
+  MyPlanListButtons,
+  MyPlanListItemWrapper,
+  MyPlanListItems,
+  MyPlanIsPublic,
+  MyPlanListCountBox,
+  MyPlanListTitle,
+  MyPlanListInviteeBox,
+  MyPlanListContentBox,
 };
