802a1ef97965c51b7bcf82ffc798a48e0ab8092b
naeun-shin <3020naaeun@gmail.com>
2024-04-22 15:50:53 +0900

    [Fix] 투표 기능 수정 및 글꼴 수정(적용 진행중)

--- src/components/commons/mainItem/MainList.tsx
+++ src/components/commons/mainItem/MainList.tsx
@@ -81,7 +81,7 @@ const Rank = styled.div`
   padding: 5px 10px;
   margin-top: 20px;
   color: #5ac8ec;
-  font-family: 'LotteriaChab', sans-serif; /* 다른 폰트 적용 */
+  font-family: 'Chab'; /* 다른 폰트 적용 */
   font-size: 30px;
   /* font-size: 3rem; */
   font-weight: bold;
